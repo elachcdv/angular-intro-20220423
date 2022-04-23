8ef359d5ae0a3f89240ef6623ae0e09c3d036987
elachcdv <elach@edu.cdv.pl>
2022-04-23 10:31:14 +0200

    creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { TodosComponent } from './todos/todos.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HelloComponent, TodosComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
